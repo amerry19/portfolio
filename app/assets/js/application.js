ab29f5bcb0a85af78529a0c902d55806bfd87305
Adam Merry <adam.martin.merry@gmail.com>
2014-05-13 23:09:42 -0700

    [CHORE] Fixed asset pipeline. Added require statements to app.js manifest file. Throws console errors but page loads all content. Will address later.

--- app/assets/js/application.js
+++ app/assets/js/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require designr
+//= require contact
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
